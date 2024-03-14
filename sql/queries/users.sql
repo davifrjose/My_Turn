d0e586efc01e9fa72736b61c9ecbd0a0097f4042
Joao Amadeu <jmamadeu2000@gmail.com>
2024-03-14 12:09:15 +0000

    clean up code base

--- sql/queries/users.sql
+++
@@ -1,7 +0,0 @@
--- name: CreateUser :one
-INSERT INTO users (id, created_at, email, name, password)
-VALUES ($1, $2, $3, $4,$5)
-RETURNING *;
-
--- name: SelectUserById :one
-Select * from users where id = $1 LIMIT 1;
